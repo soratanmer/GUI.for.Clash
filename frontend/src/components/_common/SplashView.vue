073f0a95f48f19e5307be12cf9521eb761781ba4
SORATA FUJISAWA <soratanmer@gmail.com>
2026-09-08 20:59:36 +0800

    fix: remove version display from splash screen, update .env version

--- frontend/src/components/_common/SplashView.vue
+++ frontend/src/components/_common/SplashView.vue
@@ -1,13 +1,12 @@
 <script setup lang="ts">
 import logo from '@/assets/logo'
-import { APP_TITLE, APP_VERSION } from '@/utils/env'
+import { APP_TITLE } from '@/utils/env'
 </script>
 
 <template>
   <div class="h-full flex flex-col items-center justify-center" style="--wails-draggable: drag">
     <img :src="logo" class="w-128" draggable="false" />
     <div class="text-24 font-bold mt-16 mb-24">{{ APP_TITLE }}</div>
-    <div class="py-16">{{ APP_VERSION }}</div>
     <div class="rotation">
       <slot></slot>
     </div>
